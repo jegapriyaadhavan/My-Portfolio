86499d0e698fbf1fe1e08e968c8b70a8ce027e93
Priya <54901508+jegapriyaadhavan@users.noreply.github.com>
2020-12-16 05:08:44 -0700

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,12 @@
 
+$(function(){
+	$('.repeat').click(function(){
+    	var classes =  $(this).parent().attr('class');
+        $(this).parent().attr('class', 'animate');
+        var indicator = $(this);
+        setTimeout(function(){ 
+        	$(indicator).parent().addClass(classes);
+        }, 20);
+    });
+});
+
